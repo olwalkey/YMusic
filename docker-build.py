2434b9f1805c3ef8a43fc7a4e8b391d06e411fcd
KalebSchmidlkofer <kaleb@ashbyte.com>
2024-01-03 00:22:45 -0800

    update: made not add new tag to docker image if run_docker = True

--- docker-build.py
+++ docker-build.py
@@ -1,20 +1,20 @@
 #!/bin/python3.12
 import subprocess
 
-run_docker = False
+run_docker = True
 
 with open('version') as f:
   version = f.readline()
 # Build Docker image
 subprocess.run(["docker", "build", "-t", "youtube-downloader", "."])
 
-# Tag Docker images
-subprocess.run(["docker", "tag", "youtube-downloader", "naterfute/youtube-downloader:latest"])
-subprocess.run(["docker", "tag", "youtube-downloader", f"naterfute/youtube-downloader:{version}"])
 
-# Push Docker images to Docker Hub
 if not run_docker:
+# Tag Docker images
+  subprocess.run(["docker", "tag", "youtube-downloader", "naterfute/youtube-downloader:latest"])
+  subprocess.run(["docker", "tag", "youtube-downloader", f"naterfute/youtube-downloader:{version}"])
+# Push Docker images to Docker Hub
   subprocess.run(["docker", "push", "naterfute/youtube-downloader:latest"])
   subprocess.run(["docker", "push", f"naterfute/youtube-downloader:{version}"])
 else:
-  subprocess.run(["docker", "run", "-p", "5000:5000", "naterfute/youtube-downloader:latest"])
+  subprocess.run(["docker", "run", "-p", "5000:5000", "youtube-downloader"])
