d10234b2eb68e4bb48f415683b8e1046301a2e42
KalebSchmidlkofer <kaleb@ashbyte.com>
2024-01-03 00:16:42 -0800

    update: now writes to queue db
    made def an async function and overall made code more clean

--- assets/app.py
+++ assets/app.py
@@ -1,55 +1,48 @@
-import threading
-
 import yaml
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
 from munch import munchify, unmunchify
+from downloader import Downloader, queue
 import db
-from downloader import Downloader
+import asyncio
 
 with open('./config.yaml') as stream:
-  try:
-    yamlfile = yaml.safe_load(stream)
-  except yaml.YAMLError as exc:
-    print(exc)
+    try:
+        yamlfile = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
 config = munchify(yamlfile)
 
-app = Flask(__name__) 
+app = Flask(__name__)
 api = Api(app)
 youtube = Downloader()
 
-
-
-
-
 class Download(Resource):
-  database=db.Database
-  sql = db.SQL
-  def get(self, url):
-    self.sql.new_queue(downloaded=False, url=url)
-    
+  def __init__(self):
+      self.database = db.Database()
 
+  async def get(self, url):
+    try:
+      self.database.new_queue(downloaded=False, url=url)
+      await queue.start
+      return {'message': 'Download request received and queued'}
+    except Exception as e:
+      return {'message': f'Error: {e}'}
+    
 
 class DownloadInfo(Resource):
-  def get(self):
-    data = youtube.getjson()
-    data = unmunchify(data)
-    return jsonify(data)
+    def get(self):
+        data = youtube.getjson()
+        data = unmunchify(data)
+        return jsonify(data)
 
-
-class ping(Resource):
-  def get(self):
-    
-    return {
-      'ping': 'pong',
-            }
-
+class Ping(Resource):
+    def get(self):
+        return {'ping': 'pong'}
 
 api.add_resource(Download, '/download/<string:url>')
-api.add_resource(ping, '/ping')
+api.add_resource(Ping, '/ping')
 api.add_resource(DownloadInfo, '/getjson')
 
-
 if __name__ == '__main__':
-    app.run(debug = True, host='0.0.0.0', port=config.port)
-
+    app.run(debug=False, host='0.0.0.0', port=config.port)
