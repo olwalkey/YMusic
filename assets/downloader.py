f2144548f22b0b35c5f2a1bc0ca6bde40e7f1a98
KalebSchmidlkofer <kaleb@ashbyte.com>
2024-01-25 00:01:18 -0800

    update: stealing code from the official yt-dlp github

--- assets/downloader.py
+++ assets/downloader.py
@@ -70,7 +70,30 @@ class queue:
     self.myqueue = self.db.QueueNotDone()
     return self.myqueue
 
+def format_selector(ctx):
+    """ Select the best video and the best audio that won't result in an mkv.
+    NOTE: This is just an example and does not handle all cases """
 
+    formats = ctx.get('formats')[::-1]
+
+    # acodec='none' means there is no audio
+    best_video = next(f for f in formats
+                      if f['vcodec'] != 'none' and f['acodec'] == 'none')
+
+    # find compatible audio extension
+    audio_ext = {'mp4': 'm4a', 'webm': 'webm'}[best_video['ext']]
+    # vcodec='none' means there is no video
+    best_audio = next(f for f in formats if (
+        f['acodec'] != 'none' and f['vcodec'] == 'none' and f['ext'] == audio_ext))
+
+    # These are the minimum required fields for a merged format
+    yield {
+        'format_id': f'{best_video["format_id"]}+{best_audio["format_id"]}',
+        'ext': best_video['ext'],
+        'requested_formats': [best_video, best_audio],
+        # Must be + separated list of protocols
+        'protocol': f'{best_video["protocol"]}+{best_audio["protocol"]}'
+    }
 
 class Downloader:
   debug_init(True, False)
@@ -166,6 +189,24 @@ class Downloader:
       {'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
         'preferredquality': 'None'},
+      {'add_metadata': 'True', 'key': 'FFmpegMetadata'},
+      {'already_have_thumbnail': False, 'key': 'EmbedThumbnail'}
+    ]}
+    return ydl_opts
+  
+  def video_opts(self):
+    ydl_opts = {
+      # 'ratelimit': 500000, # Kilobytes
+      'logger': MyLogger(),
+      'breakonexisting': True,
+      'progress_hooks': [self.progress_hook],
+      'postprocessor_hooks': [self.postprocessor_hooks],
+      'writethumbnail': True,
+      'outtmpl': 'videos/%(playlist_title)s/%(playlist_autonumber)s - %(title)s.%(ext)s',
+      'skip_broken': True,
+      'ignoreerrors': True,
+      'format': format_selector,
+      'postprocessors': [
       {'add_metadata': 'True', 'key': 'FFmpegMetadata'},
       {'already_have_thumbnail': False, 'key': 'EmbedThumbnail'}
     ]}
