cd64d3d9606d07560c1fc778ae17c6ea64bd808d
KalebSchmidlkofer <kaleb@ashbyte.com>
2024-09-27 13:39:54 -0700

    Updated Decorators to be actually useful
    time_function > Performance
    
    Performance Measures stats like
    Current and peak Mem usage, and time to execute
    
    WIP:
    
    check_database_con:
      - Once it fails to connect to database 3 time's stop reconnecting and
        just start returning "Database connection is failed please restart
        server." This is to prevent memory leaks of my bad code
      -  Also ensures the database connection is live that way no Data is
         lost

--- app.py
+++ app.py
@@ -5,6 +5,13 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 
+from apscheduler.schedulers.background import BackgroundScheduler
+
+from functools import wraps
+import tracemalloc
+from time import perf_counter
+
+
 from time import time
 
 from datetime import datetime
@@ -29,7 +36,9 @@ def debug_init(trace, debug):
     pass
 
 
-Database_con = False
+Database_con: bool = False
+
+DatabaseConFailures: int = 0
 
 shared_data = {
     'info': {
@@ -42,17 +51,57 @@ shared_data = {
 }
 
 
-def scanDatabase():
-    pass
+def check_database_con(DatabaseConFailures):
+    def check_database_con(func):
+        async def wrapper(*args, **kwargs):
+            if Database_con:
+                result = func(*args, **kwargs)
+                return result
+            else:
+                logger.error(
+                    '''There Was A problem connecting to database! Ensure the server is running and your Credentials are correct! If That doesn't work Run server in debug mode!''')
+                DatabaseConFailures += 1
+                print(DatabaseConFailures)
+
+                return False
+        return wrapper
+    return check_database_con
 
 
-def tick():
-    print('Tick! The time is: %s' % datetime.now())
+def performance(func):
+    async def wrapper(*args, **kwargs):
+        tracemalloc.start()
+        start_time = perf_counter()
+        func(*args, **kwargs)
+        current, peak = tracemalloc.get_traced_memory()
+        finish_time = perf_counter()
+        logger.trace(f'Function: {func.__name__}')
+        logger.trace(f'Method: {func.__doc__}')
+        logger.trace(f'Memory usage:\t\t {current / 10**6:.6f} MB \n'
+                     f'Peak memory usage:\t {peak / 10**6:.6f} MB ')
+        logger.trace(f'Time elapsed is seconds: {
+                     finish_time - start_time:.6f}')
+        logger.trace(f'{"-"*40}')
+        tracemalloc.stop()
+
+
+@check_database_con
+@performance
+async def scanDatabase():
+    fetched = utils.interaction.fetchNextItem()
+
+    if not fetched:
+        return "There was A problem connecting to the database! Please check server logs"
+    else:
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     debug_init(True, False)
+    scheduler = BackgroundScheduler()
+    # Check and fetch next database item
+    scheduler.add_job(scanDatabase, 'interval', seconds=5)
+    scheduler.start()
     yield
     print('Shutting down...')
 
@@ -73,49 +122,32 @@ app.add_middleware(
 )
 
 
-def check_database_con(func):
-    async def wrapper(*args, **kwargs):
-        if Database_con:
-            result = func(*args, **kwargs)
-        else:
-            logger.error(
-                '''There Was A problem connecting to database! Ensure the server is running and your Credentials are correct! If That doesn't work Run server in debug mode!''')
-            return "There was A problem connecting to the database! Please check server logs"
-        return result
-    return wrapper
-
-
-def time_function(func):
-    async def wrapper(*args, **kwargs):
-        start = time()
-        func(*args, **kwargs)
-        end = time()
-        logger.trace(f'{func.name} Took {end-start} To Execute')
-
-
 # def get_db():
 #    db=SessionLocal
 
 # Make use authentication
-@check_database_con
-@time_function
+@check_database_con(DatabaseConFailures)
+@performance
 @app.post('/download/{url}/')
 async def download_route(url: str):
+    """Request A Download from the Database"""
     return utils.youtube.start_download(url=url)
 
 
-@check_database_con
-@time_function
+@check_database_con(DatabaseConFailures)
+@performance
 @app.get('/ping')
 async def ping():
+    """Ping the server and respond in kind"""
     return {'ping': 'pong'}
 
 
 # Make use authentication
-@check_database_con
-@time_function
+@check_database_con(DatabaseConFailures)
+@performance
 @app.get('/getjson')
 async def get_json():
+    """Get current downloading information"""
     # data = youtube.getjson()
     data = unmunchify(shared_data)
     return JSONResponse(content=data)
