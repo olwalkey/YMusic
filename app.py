f7833fe11c25b75505915f36e5d1bda5bbdb3781
KalebSchmidlkofer <kaleb@ashbyte.com>
2024-09-27 12:38:47 -0700

    feat: added decorates to track function time and database connection

--- app.py
+++ app.py
@@ -5,15 +5,17 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 
+from time import time
 
 from datetime import datetime
 
 from munch import unmunchify
+from loguru import logger
 
 
 import utils
 
-download_lock = False
+Database_con = False
 
 shared_data = {
     'info': {
@@ -56,13 +58,34 @@ app.add_middleware(
 )
 
 
+def check_database_con(func):
+    async def wrapper(*args, **kwargs):
+        if Database_con:
+            result = func(*args, **kwargs)
+        else:
+            return "There was A problem connecting to the database! Please check server logs"
+        return result
+    return wrapper
+
+
+def time_function(func):
+    async def wrapper(*args, **kwargs):
+        start = time()
+        func(*args, **kwargs)
+        end = time()
+        logger.debug(f'{func.name} Took {end-start} To Execute')
+
+
 # def get_db():
 #    db=SessionLocal
 
 # Make use authentication
+
+
+@check_database_con
+@time_function
 @app.post('/download/{url}/')
 async def download_route(url: str):
-
     return utils.youtube.start_download(url=url)
 
 
