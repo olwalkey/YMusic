06906223b90f3611628bd76f35aa5fb13b3a5f5f
KalebSchmidlkofer <kaleb@ashbyte.com>
2024-11-26 19:55:28 -0800

    Began rewrite of db.py to support unit tests

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,34 +1,43 @@
-from .db import interactions
+from .db import interactions, DBInfo
 from alembic.config import Config
 from alembic import command
-from loguru import logger
-from typing import Any
 from robyn import Robyn
-
 from .config import config
+from loguru import logger
+
+#from .downloader import Downloader, robyn
+
+def migrateDb(
+    engineType: DBInfo,
+    username: str = config.db.user,
+    password: str = config.db.password,
+    host: str = config.db.host,
+    port: int = config.db.port,
+    database: str = config.db.db
+) -> int:
+    alembic_cfg = Config()
+    alembic_cfg.set_main_option("script_location", "utils/alembic")
+    alembic_cfg.set_main_option("prepend_sys_path", ".")
+    if not engineType['database'] == "sqlite":
+        alembic_cfg.set_main_option("sqlalchemy.url", f'{engineType["database"]}://{username}:{password}@{host}:{port}/{database}')
+    else:
+        alembic_cfg.set_main_option("sqlalchemy.url", f'{engineType["database"]}:///{database}')
+
+    try:
+        command.ensure_version(alembic_cfg)
+        command.upgrade(alembic_cfg, "head")
+        return 1
+    except Exception as e:
+        logger.error(e)
+        return 0
+
+#interaction = interactions()
+#interaction.connect()
 
 
 
+#def initapp(app: Robyn):
+    #robyn(app)
 
 
-
-alembic_cfg = Config()
-alembic_cfg.set_main_option("script_location", "utils/alembic")
-alembic_cfg.set_main_option("prepend_sys_path", ".")
-alembic_cfg.set_main_option("sqlalchemy.url", f'postgresql://{config.db.user}:{config.db.password}@{config.db.host}:{config.db.port}/{config.db.db}')
-
-
-command.ensure_version(alembic_cfg)
-command.upgrade(alembic_cfg, "head")
-
-interaction = interactions()
-interaction._connect()
-
-from .downloader import Downloader, robyn
-
-
-def initapp(app: Robyn):
-    robyn(app)
-    
-
-youtube = Downloader()
+#youtube = Downloader()
--- utils/db.py
+++ utils/db.py
@@ -1,9 +1,11 @@
-from sqlalchemy import create_engine, select, update
+from sqlalchemy import except_, select, update
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import func
 from sqlalchemy.exc import DuplicateColumnError, DBAPIError, OperationalError
 from urllib.parse import urlparse, parse_qs
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, AsyncEngine
 
+from typing import TypedDict
 
 from bcrypt import gensalt
 import argon2
@@ -17,184 +19,171 @@ from . import models as Tables
 from .models import Base
 
 
-def spliturl(url: list):
-    """Splits url and returns the youtube video/playlist id
+class DBInfo(TypedDict):
+    database: str
+    driver: str
 
-    Args:
-        url (string): Youtube url Eg: https://www.youtube.com/watch?v=sVJEaYNOUNw
 
-    Returns:
-        string: End of youtube url
-    """
-    parsed_url = urlparse(url)  # type: ignore
+class DBTypes():
+    postgresql: DBInfo = {"database": "postgresql", "driver": "asyncpg"}
+    mysql: DBInfo = {"database": "mysql", "driver": "aiomysql"}
+    mariadb: DBInfo = {"database": "mysql", "driver": "aiomysql"}
+    sqlite: DBInfo = {"database": "sqlite", "driver": "aiosqlite"}
 
-    query_params = parse_qs(parsed_url.query)
-    video_id = query_params.get('v')
-    playlist_id = query_params.get('list')
-    if not playlist_id == None:
-        url.append(playlist_id)
-    else:
-        url.append(video_id)
-    return url
 
 
 class interactions:
-    def __init__(self):
-        self.fetchNextDownload = (
-            select(Tables.Requests)
-            .where(Tables.Requests.queue_status == 'queued')
-            .order_by(Tables.Requests.queue_status, Tables.Requests.create_time.desc())
-        )
+    dbtest:int = 0
+    engine: AsyncEngine
+    engineType: DBInfo = DBTypes.postgresql
 
-    def _connect(self):
-        self.engine = create_engine(
-            f'postgresql+psycopg2://{config.db.user}:{config.db.password}@{
-                config.db.host}:{config.db.port}/{config.db.db}',
-            pool_size=5,
-            max_overflow=0,
-            echo=False,
-            connect_args={"options": f"-c timezone={config.db.timezone}"}
-        )
-        self.conn = self.engine.connect()
-        self.userConn = self.engine.connect()
-        self.session = sessionmaker(bind=self.engine, expire_on_commit=False)
+    username: str = "postgres"
+    password: str = "supersecurepass"
+    host: str = "localhost"
+    port: int = 5432
+    database: str = "youtube"
+    
+    @classmethod
+    async def setEnginetype(cls, etype: DBInfo):
+        cls.engineType = etype
+    
+    @classmethod
+    async def setUsername(cls, username) -> None:
+        cls.username = username
 
-    def check_conn(self) -> dict:
+    @classmethod
+    async def setPassword(cls, password) -> None:
+        cls.password = password
+    
+    @classmethod
+    async def setHost(cls, host) -> None:
+        cls.host = host
+    
+    @classmethod
+    async def setPort(cls, port) -> None:
+        cls.port = port
+    
+    @classmethod
+    async def setDatabase(cls, database) -> None:
+        cls.database = database
+
+    @classmethod
+    async def connect(cls) -> AsyncEngine:
+        """
+            Connects to the database and persist a connection using a connection pool
+            ---
+        """
         try:
-            conn = self.engine.connect()
-            conn.close()
-            return {"conn": True, "type": None, "error": None}
-        except OperationalError as e:
-            return {"conn": False, "type": "DBAPIError",  "error": e}
-        except DBAPIError as e:
-            return {"conn": False, "type": "DBAPIError", "error": e}
-
-    def createEntry(self, id):
-        """Creates An Entry in the Requests Table"""
-        try:
-            with self.session() as session:
-                new_request = Tables.Requests(url=id)
-                session.add(new_request)
-                session.commit()
-
-                return {
-                    'data': {
-                        'message': 'Download request received and queued',
-                        'error': None
-                    }
-                }
-        except DuplicateColumnError as e:
-            return {
-                'data': {
-                    'message': 'Duplicate Entry. Link already exists!',
-                    'error': e
-                }
-            }
-
-        except Exception as e:
-            return {
-                'data': {
-                    'message': 'An unknown error occurred',
-                    'error': e
-                }
-            }
-
-
-    def fetchNextItem(self):
-        """Returns next item to download"""
-        fetch = self.conn.execute(statement=self.fetchNextDownload)
-        if fetch is None:
-            return None
-        return fetch.fetchone()
-
-    def markVideoDownloaded(
-        self,
-        playlisturl,
-        url,
-        title,
-        download_path,
-        elapsed
-    ):
-        """Adds An entry in Downloaded Table with the downloaded item"""
-        try:
-            with self.session() as session:
-                logger.debug(session)
-                related_playlist = session.query(
-                    Tables.Requests).filter_by(url=playlisturl).first()
-                if not related_playlist:
-                    logger.error(f"No playlist found for url {url}")
-                new_download = Tables.Downloaded(
-                    title=title,
-                    url=url,
-                    path=download_path,
-                    elapsed=elapsed,
-                    playlist=related_playlist)
-                logger.debug(new_download)
-                session.add(new_download)
-                session.commit()
-        except Exception as e:
-            logger.error(e)
-
-    def markPlaylistDownloaded(self, url, title, extractor):
-        """Marks A Playlist as completely Downloaded"""
-        with self.session() as session:
-            logger.debug("")
-            self.updateDownloaded = (
-                update(Tables.Requests)
-                .where(Tables.Requests.url == url)
-                .values(
-                    title=title,
-                    queue_status="completed",
-                    downloaded_time=func.now(),
-                    extractor=extractor,
+            if not cls.engineType['database'] == "sqlite":
+                cls.engine = create_async_engine(
+                    url=f"{cls.engineType['database']}+{cls.engineType['driver']}://{cls.username}:{cls.password}@{cls.host}:{cls.port}/{cls.database}",
+                    pool_size=10,
+                    max_overflow=10,
+                    pool_timeout=30,
+                    echo=True,
                 )
-            )
-            logger.debug(self.updateDownloaded)
-            session.execute(self.updateDownloaded)
-            session.commit()
+            else:
+                cls.engine = create_async_engine(
+                    url=f"{cls.engineType['database']}+{cls.engineType['driver']}:///{cls.database}.sqlite",
+                    pool_size=10,
+                    max_overflow=10,
+                    pool_timeout=30,
+                    echo=True,
+                )
 
-    def fetchUser(self, user):
-        """Checks if A user exists by username"""
-        fetchedUser = (
-            select(Tables.Users)
-            .where(Tables.Users.username == user))
-        try:
-            fetch = self.conn.execute(statement=fetchedUser)
-            return fetch.fetchone()
 
         except Exception as e:
+            logger.error("Failed to connect to the database!")
             logger.error(e)
-            return None
+        if not isinstance(cls.engine, AsyncEngine):
+            logger.error(f"engine Responded with type: {type(cls.engine)}")
+            exit()
 
-    def new_user(self, user: str, user_pass: str):
-        """Created A new user using username and password"""
-        myfetch = self.fetchUser(user)
-        if myfetch == None:
-            myfetch = []
+        return cls.engine
 
-        if len(myfetch) != 0:  # type: ignore
-            logger.error('User already exists')
-            return "User already Exists"
-        else:
-            user = user.lower()
-            user_salt = gensalt()
-            ph = argon2.PasswordHasher()
-            hash = ph.hash(password=user_pass, salt=user_salt)
-            with self.session() as session:
-                new_user = Tables.Users(username=user, password=hash,
-                                        salt=user_salt, admin=False)
-                session.add(new_user)
-                session.commit()
-                session.refresh(new_user)
-                return new_user
 
-    def verify_user(self, user: str, password: str):
-        """Verify the sent password to the stored password"""
-        user_hash = self.fetchUser(user)
-        if user_hash is None:
-            return False
-        hashed_password = user_hash[2]
-        ph = argon2.PasswordHasher()
-        ph.verify(hashed_password, password)
-        ph.check_needs_rehash(hashed_password)
-        return True, user_hash[1]
+    @classmethod
+    async def reconnect(cls) -> None:
+        """
+            Attempts to reconnect to the database if testcon fails
+            ---
+        """
+        pass
+
+    @classmethod
+    async def testcon(cls) -> int:
+        """
+            Tests connection to the datbabase
+            ---
+            Returns 0 if failed
+            Returns 1 if Success
+        """
+
+        try:
+            async with cls.engine.connect() as connection:
+                return 1
+        except Exception as e:
+            print("Connection failed:", str(e))
+            return 0
+
+    @classmethod
+    async def createEntry(cls) -> None:
+        """
+            Creates a new entry in the requests table to download once it's called in queue
+            ---
+        """
+        pass
+
+
+    @classmethod
+    async def fetchNextItem(cls) -> None:
+        """
+            Fetches next eligible item for download from the datbase
+            ---
+        """
+        pass
+
+    @classmethod
+    async def newDownloaded(cls) -> None:
+        """
+            Creates a new entry in the Downloaded Table
+            and marks it downloaded with all relevent info
+            ---
+        """
+        pass
+
+    @classmethod
+    async def playlistDownloaded(cls) -> None:
+        """
+            Takes a playlist id and set's it's status to completed in the db  
+
+            ---
+        """
+        pass
+
+    @classmethod
+    async def newUser(cls) -> None:
+        """
+            Creates a new entry in the database for a new user
+            ---
+        """
+        pass
+
+    @classmethod
+    async def fetchUser(cls) -> None:
+        """
+        Fetches a user from the database
+        ---
+        """
+        pass
+
+
+    @classmethod
+    async def verifyUser(cls) -> None:
+        """
+            Verify A username and hash against it in the database
+            ---
+        """
+        pass
+
+
